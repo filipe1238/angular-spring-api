4fd1fb7f35882e992df5bcf8482644ad6979ad7b
Filipe <83041450+filipe1238@users.noreply.github.com>
2023-04-18 10:49:32 -0300

    added default configs

--- demo/src/main/java/com/example/demo/config/RedisConfig.java
+++ demo/src/main/java/com/example/demo/config/RedisConfig.java
@@ -14,31 +14,31 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.util.UUID;
 
 @Configuration
-@AutoConfigureAfter(RedisAutoConfiguration.class)
-@EnableCaching
-public class RedisConfig {
+class RedisConfig {
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-//        redisStandaloneConfiguration.setPort(6739);
+        // redisStandaloneConfiguration.setHostName("127.0.0.1");
+        // redisStandaloneConfiguration.setPort(6379);
+        // redisStandaloneConfiguration.setDatabase(1);
+        // redisStandaloneConfiguration.setUsername("password");
+        // redisStandaloneConfiguration.setPassword("password");
+
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
+
     @Bean
     public RedisTemplate<UUID, Object> redisTemplate() {
-        RedisTemplate<UUID, Object> template =
-                new RedisTemplate<>();
-        template.setConnectionFactory(
-                jedisConnectionFactory());
-        template.setKeySerializer(
-                new StringRedisSerializer());
-        template.setHashKeySerializer(
-                new StringRedisSerializer());
-        template.setHashKeySerializer(
-                new JdkSerializationRedisSerializer());
-        template.setValueSerializer(
-                new JdkSerializationRedisSerializer());
+        RedisTemplate<UUID, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
         template.setEnableTransactionSupport(true);
         template.afterPropertiesSet();
+
         return template;
     }
 }
