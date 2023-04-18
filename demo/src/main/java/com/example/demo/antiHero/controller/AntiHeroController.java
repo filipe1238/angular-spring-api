81831ccf7c6186e3819268ad5909d30016a7fda7
Filipe <83041450+filipe1238@users.noreply.github.com>
2023-04-18 10:49:06 -0300

    added cors support

--- demo/src/main/java/com/example/demo/antiHero/controller/AntiHeroController.java
+++ demo/src/main/java/com/example/demo/antiHero/controller/AntiHeroController.java
@@ -9,7 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -17,6 +17,7 @@ import java.util.stream.StreamSupport;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin(allowedHeaders = "Content-type")
 @RequestMapping("api/v1/anti-heroes")
 public class AntiHeroController {
     private final AntiHeroService service;
@@ -60,12 +61,14 @@ public class AntiHeroController {
     public void deleteAntiHeroById(@PathVariable("id") UUID id) {
         service.removeAntiHeroById(id);
     }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
-    public List<AntiHeroDto> getAntiHeroes() {
+    public List<AntiHeroDto> getAntiHeroes(Pageable pageable) {
+        int toSkip = pageable.getPageSize() *
+                pageable.getPageNumber();
         var antiHeroList = StreamSupport
-                .stream(service.findAllAntiHeroes().spliterator(),
-                        false)
+                .stream(service.findAllAntiHeroes().spliterator(),false)
+                .skip(toSkip).limit(pageable.getPageSize())
                 .collect(Collectors.toList());
         return antiHeroList
                 .stream()
