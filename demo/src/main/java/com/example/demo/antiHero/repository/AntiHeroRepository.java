dc7a7b824f187160a689ca9e80165c54f74758ca
filipe1238 <filipe12388@hotmail.com>
2023-04-18 22:02:52 -0300

    removed unused entity

--- demo/src/main/java/com/example/demo/antiHero/repository/AntiHeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.antiHero.repository;
-
-import com.example.demo.antiHero.entity.AntiHeroEntity;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface AntiHeroRepository extends CrudRepository<AntiHeroEntity, UUID> {
-}
