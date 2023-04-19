c306bd193f09e15cbca7fb47b7e7a156571fba03
Filipe <83041450+filipe1238@users.noreply.github.com>
2023-04-19 12:29:26 -0300

    get methos working

--- front-app-master/src/app/carrinho/carrinho.component.ts
+++ front-app-master/src/app/carrinho/carrinho.component.ts
@@ -2,29 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { Carrinho } from './carrinho';
 import { CarrinhoService } from './lista.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { v1 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
-/*   template: `
+  /*   template: `
   <h1>hello world</h1>
   `, */
-  styleUrls: ['./carrinho.component.scss']
+  styleUrls: ['./carrinho.component.scss'],
 })
 export class CarrinhoComponent implements OnInit {
-
-
-  ngOnInit(): void {
-  }
   public carrinhos: Carrinho[];
+  public carrinhoTeste: Carrinho;
+  public test: String = 'teste';
+
+  async ngOnInit() {
+    console.log(this.carrinhos);
+    /* this.getCarrinho("3d69ec18-88ba-4ca7-abb9-2b5ac1b82b65"); */
+    this.getCarrinhos();
+  }
 
   constructor(private service: CarrinhoService) {}
 
-  public getCarrinho(): void {
+  public getCarrinhos(): void {
     this.service.getCarrinhos().subscribe(
-      (response: Carrinho[]) => {
+      (response: any) => {
+        console.log(response);
         this.carrinhos = response;
-        console.log(this.carrinhos);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+  public getCarrinho(id: uuid): void {
+    this.service.getCarrinho(id).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.carrinhoTeste = response;
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
--- front-app-master/src/app/carrinho/lista.service.ts
+++ front-app-master/src/app/carrinho/lista.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Carrinho } from './carrinho';
 import { environment } from 'src/environments/environment';
+import { v1 as uuid } from 'uuid';
 
 @Injectable({providedIn: 'root'})
 export class CarrinhoService {
@@ -11,18 +12,21 @@ export class CarrinhoService {
   constructor(private http: HttpClient){}
 
   public getCarrinhos(): Observable<Carrinho[]> {
-    return this.http.get<Carrinho[]>(`${this.apiServerUrl}/carrinhos/`);
+    return this.http.get<Carrinho[]>(`${this.apiServerUrl}carrinhos`);
+  }
+  public getCarrinho(id: uuid): Observable<Carrinho[]> {
+    return this.http.get<Carrinho[]>(`${this.apiServerUrl}carrinhos/${id}`);
   }
 
   public addEmployee(employee: Carrinho): Observable<Carrinho> {
-    return this.http.post<Carrinho>(`${this.apiServerUrl}/employee/add`, employee);
+    return this.http.post<Carrinho>(`${this.apiServerUrl}employee/add`, employee);
   }
 
   public updateEmployee(employee: Carrinho): Observable<Carrinho> {
-    return this.http.put<Carrinho>(`${this.apiServerUrl}/employee/update`, employee);
+    return this.http.put<Carrinho>(`${this.apiServerUrl}employee/update`, employee);
   }
 
   public deleteEmployee(employeeId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+    return this.http.delete<void>(`${this.apiServerUrl}employee/delete/${employeeId}`);
   }
 }
