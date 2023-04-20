c07f66259587e5c6ad036a8d1d7e6236c61594e4
Filipe <83041450+filipe1238@users.noreply.github.com>
2023-04-20 11:24:24 -0300

    changed packages

--- demo/src/main/java/com/example/demo/utils/SuperClassDto.java
+++ demo/src/main/java/com/example/demo/utils/SuperClassDto.java
@@ -1,4 +1,4 @@
-package com.example.demo.produto.dto;
+package com.example.demo.utils;
 
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -9,6 +9,6 @@ import java.util.Map;
 
 @MappedSuperclass
 @Getter @Setter
-public class SuperClassDtoProd {
+public class SuperClassDto {
     private Map<String, Object> changedAttrs = new HashMap<>();
 }
